c01b6f29906c3e83b3648e707628002594c6a092
Adolf Macro <87756378+AdolfMacro@users.noreply.github.com>
2022-07-28 18:05:18 +0430

    Update updater.sh

--- tools/updater.sh
+++ tools/updater.sh
@@ -1,6 +1,6 @@
 crd=pwd
 cd /tmp/
 git clone https://github.com/AdolfMacro/lordrat.git
-sudo -u root cp -R lordrat/* /usr/src/lordrat/
+sudo -u root cp -R lordrat/* /usr/src/lordRAT/
 sudo -u root rm -rf lordrat
 cd $prc
